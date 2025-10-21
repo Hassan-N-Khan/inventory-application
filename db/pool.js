c5ff558811193794808e9d4f0d15ca4ee6a2d75c
Hassan Khan <hassannadeemkhan@gmail.com>
2025-10-21 18:46:45 -0400

    added password as well for deploy

--- db/pool.js
+++ db/pool.js
@@ -7,5 +7,6 @@ module.exports = new Pool({
   host: process.env.PG_HOST, // or wherever the db is hosted
   user: process.env.PG_USER,
   database: process.env.PG_NAME,
-  port: process.env.PG_PORT // The default port
+  port: process.env.PG_PORT, // The default port
+  password: process.env.PG_PASSWORD
 });
