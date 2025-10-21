7e29c6db82c536cee4d7d04b2d58417f96ddbf52
Hassan Khan <hassannadeemkhan@gmail.com>
2025-10-21 18:45:04 -0400

    added env variable for deploy on render

--- db/pool.js
+++ db/pool.js
@@ -1,10 +1,11 @@
 const {Pool} = require('pg');
+require('dotenv').config();
 
 // All of the following properties should be read from environment variables
 // We're hardcoding them here for simplicity
 module.exports = new Pool({
-  host: "localhost", // or wherever the db is hosted
-  user: "hassan",
-  database: "movies",
-  port: 5432 // The default port
+  host: process.env.PG_HOST, // or wherever the db is hosted
+  user: process.env.PG_USER,
+  database: process.env.PG_NAME,
+  port: process.env.PG_PORT // The default port
 });
